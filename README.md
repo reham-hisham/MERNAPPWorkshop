1196fe8e79dcc06a2e326f1c0e18911f673e1e92
RehamMaloom <59805450+reham-hisham@users.noreply.github.com>
2022-11-18 11:22:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# restapiworkshop" 
+"# MERNAPPWorkshop" 
---
+++ mongoose/models/user.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+const user = mongoose.Schema({
+
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            validation(val){
+                if(!validator.isEmail(val)){
+                    throw new Error("not valid email")
+                }
+            }
+        },
+      
+        
+        password:{
+            type:String,
+            required:true,
+            validation(val){
+                if(!validator.isStrongPassword(val)){
+                    throw new Error("try strong password")
+                }
+            }
+        },
+        userRole:{
+            type:String,
+            enum:["admin","user"],
+            default:"user"
+        },
+        tokens:[
+            {
+                token:{
+                    type:String
+                }
+            }
+        ]
+    
+
+
+})
+user.pre("save",async function(){
+
+    if(this.isModified("password")){
+
+        this.password = await bcrypt.hash(this.password, 4)
+      
+    }
+    
+})
+module.exports= mongoose.model("user",user)
